c7ceae95493617ab4b0b1c9a08cbb24d186ac256
Barry <renault194@hotmail.com>
2015-05-14 00:04:17 +1000

    add comments about the parameters of constructing a game

--- src/Gameplatform.java
+++ src/Gameplatform.java
@@ -3,6 +3,17 @@ import Gomoku.ChessGameDirector;
 import Gomoku.GomokuGameBuilder;
 import Gomoku.GomokuPlay;
 
+/**
+ * game entry point, options for passing the builder as follows:
+ * @param mode: 
+ * 1 - player just has move option , could not redo or undo
+ * 2 - player can move, redo and undo
+ * @param timout:
+ * max time of each round
+ * less than 10 seconds would be treated as no time limit
+ * @author Barry
+ * @since 2015-5-9
+ */
 public class Gameplatform {
 
 	private static GomokuPlay game = null;
